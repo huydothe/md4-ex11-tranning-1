558981a0bcd1f146fab81e6516940b85019905ba
Your Name <you@example.com>
2022-09-19 20:18:15 +0700

    bt

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,58 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const winston_1 = require("./src/logger/winston");
+const Sentry = __importStar(require("@sentry/node"));
+const Tracing = __importStar(require("@sentry/tracing"));
 const port = 3000;
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.json());
-app.get('/', (req, res) => {
-    try {
-        res.end("<h1>Hello winston!</h1>");
-        throw new Error("Error test winston");
-    }
-    catch (err) {
-        winston_1.logger.error(err);
-    }
+Sentry.init({
+    dsn: "https://66328cdafce8450e80b242f671b91eb9@o1417692.ingest.sentry.io/6760360",
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app })
+    ],
+    tracesSampleRate: 1.0
+});
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+app.get("/", function rootHandler(req, res) {
+    res.end("Hello world!");
+});
+app.use(Sentry.Handlers.errorHandler());
+app.use(function onError(err, req, res, next) {
+    res.statusCode = 500;
+    res.end(res.sentry + '\n');
+});
+app.get('/debug-sentry', function mainHandle(req, res) {
+    throw new Error("My first sentry error!!");
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from "body-parser";
 import {logger} from "./src/logger/winston";
+import * as Sentry from"@sentry/node";
+import * as Tracing from"@sentry/tracing";
 
 const port = 3000;
 
@@ -8,15 +10,43 @@ const app =express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    try {
-        res.end("<h1>Hello winston!</h1>")
-        throw new Error("Error test winston");
-    } catch (err) {
-        logger.error(err)
-    }
+// app.get('/', (req, res) => {
+//     try {
+//         res.end("<h1>Hello winston!</h1>")
+//         throw new Error("Error test winston");
+//     } catch (err) {
+//         logger.error(err)
+//     }
+// })
+
+
+Sentry.init({
+    dsn : "https://66328cdafce8450e80b242f671b91eb9@o1417692.ingest.sentry.io/6760360",
+    integrations: [
+        new Sentry.Integrations.Http({tracing:true}),
+        new Tracing.Integrations.Express({app})
+    ],
+    tracesSampleRate :1.0
 })
 
+app.use(Sentry.Handlers.requestHandler());
+
+app.use(Sentry.Handlers.tracingHandler());
+
+app.get("/", function rootHandler(req, res) {
+    res.end("Hello world!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next){
+    res.statusCode = 500;
+    res.end(res.sentry + '\n')
+})
+
+app.get('/debug-sentry',function mainHandle(req,res){
+    throw new Error("My first sentry error!!")
+})
 
 app.listen(port,()=>{
     console.log(`Server is running at http://localhost:${port}`);
